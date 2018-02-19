f8810b7f1111db702686a1231d4cfe948f56263c
John Toomey <crosswired@gmail.com>
2018-02-19 19:19:08 +0000

    Count the number of a given set of characters in a file
    To figure out which characters occur most frequently in a source file
    and therefore should be easily accessed on a recused size keyboard
    
    Signed-off-by: John Toomey <crosswired@gmail.com>

---
+++ charcount/charcount.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	b, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Println("err")
+	}
+	str := string(b)
+
+	count := make(map[rune]int)
+	runes := ".,_!£$%^&*(~{+'"
+
+	// Add all of the characters to the map with value 0
+	for _, r := range runes {
+		count[r] = 0
+	}
+
+	// Loop over the string and count the values
+	for _, r := range str {
+		_, ok := count[r]
+		if ok {
+			count[r]++
+		}
+	}
+
+	// Print all of the counts
+	for k, v := range count {
+		fmt.Printf("%q %d\n", k, v)
+	}
+}
